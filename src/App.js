94d806be78e5b122251c8a1b2c1bd1ab5de486cf
Ashley Hay <hayashley@msn.com>
2019-07-24 12:54:21 -0700

    weather app

--- src/App.js
+++ src/App.js
@@ -4,66 +4,92 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import SearchIcon from '@material-ui/icons/Search';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import moment from 'moment'
+import { Bar } from 'react-chartjs-2'
 
 
 
 class App extends React.Component {
   state = {
-    memes: [],
+    weather: null,
     loading: false,
     text: '',
   }
 
-  getMemes = async (e) => {
+  getWeather = async (e) => {
     e.preventDefault() // no refreshing when searching
-    this.setState({ loading: true })
-    var key = 'wP9iA7nFzpyz2JQtDNwrbBDfkAqpwTPP'
-    var url = `http://api.giphy.com/v1/gifs/search?q=${this.state.text}&api_key=${key}`
+    this.setState({ loading: true, weather: null })
+    var key = 'bfc8f5b2eabca4875bc14b9db2704655'
+    var url = `http://api.openweathermap.org/data/2.5/forecast?q=${this.state.text}&units=imperial&APPID=${key}`
     var r = await fetch(url)
     var json = await r.json()
-    this.setState({ memes: json.data, loading: false, text: '' })
+    console.log(json)
+
+    if (r.status===200){
+    //set thet weather in state and loading to false and text is an empty string
+    this.setState({ weather: json.list, loading: false, text: '', error:null})
+    } else{
+      this.setState({error: json.message, loading: false,})
+    }
   }
 
   render() {
-    var { memes, loading, text } = this.state
+    //extract the peices of state  of state so we can 
+    var { weather, loading, text, error } = this.state
+    console.log(weather)
+
+    var data
+    if (weather) {
+
+      data = {
+        labels: weather.map(w => moment(w.dt*1000).format('llll')),
+        datasets: [{
+          label: 'Temperature',
+          data: weather.map(w => w.main.temp),
+          borderWidth: 1,
+          backgroundColor: 'rgba(132,99,255,0.2)',
+          borderColor: 'rgba(132,99,255,1)',
+          hoverBackgroundColor: 'rgba(132,99,255,0.4)',
+          hoverBorderColor: 'rgba(132,99,255,1)',
+
+        }]
+      }
+    }
     return (
-      <div className="App"> search_Memes
-        <form className="App-header" onSubmit={this.getMemes}> 
-          <TextField className= ".MuiInputBase-input"
-          value={text}
-          label="Search for Memes"
-          variant="outlined"    
-        
-          onChange={e => this.setState({ text: e.target.value })} 
-        style = {{width:'100%', marginLeft: 8}}
-         />
+      <div className="App"> search_weather
+        <form className="App-header" onSubmit={this.getweather}>
+          <TextField className=".MuiInputBase-input"
+            value={text}
+            label="Search for weather"
+            variant="outlined"
+
+            onChange={e => this.setState({ text: e.target.value })}
+            style={{ width: '100%', marginLeft: 8 }}
+          />
           <Button variant=" contained"
-          color="primary"
-          type="submit"
-          disabled={loading || !text}
-          style = {{ width:150, margin: '0 10px', height: '75', color: 'white'}} >
-          <SearchIcon style={{margnRight:8}}/>
-          Search
-          {loading && <CircularProgress size={24}  />}
-           </Button>
+            color="primary"
+            type="submit"
+            disabled={loading || !text}
+            style={{ width: 150, margin: '0 10px', height: '75', color: 'white' }} >
+            <SearchIcon style={{ margnRight: 8 }} />
+            Search
+          {loading && <CircularProgress size={24} />}
+          </Button>
         </form>
 
-      <main>
-            {memes.map(meme=>{
-              return <Meme key={meme.id} meme={meme} />
-            })}
-      </main>
+        <main>
+          {data && <Bar 
+            data={data}
+            width={100}
+            height={50}
+         /*  options={{ maintainAspectRatio: false}}*/
+
+    />}
+    {error && <div style= {{color: 'red'}}>{error}</div>}
+        </main>
       </div>
-  );
+    );
   }
 }
 
-
-function Meme(props){
-  const {meme} = props
-  const url = meme.images.fixed_height.url
-  return (<div className="meme-wrap" onClick={()=>window.open(url, '_blank')}>
-    <img height="200" alt="meme" src={url} />
-  </div>)
-}
 export default App;
